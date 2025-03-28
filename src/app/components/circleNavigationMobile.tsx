a5b86b81a99df4b4985f57b27fbb6e1a87171e67
nhl1991 <laslark1991@gmail.com>
2025-03-28 18:36:24 +0900

    updated component for mobile.

--- src/app/components/circleNavigationMobile.tsx
+++ src/app/components/circleNavigationMobile.tsx
@@ -9,11 +9,11 @@ export default function NavigationMobile() {
     const cirlceCount = 8; // Just change this value to add more circle and adjust size in page.module.css file
 
     return (
-        <div id="ring" className='w-full h-max p-4 grid grid-cols-3 items-center justify-center bg-slate-400/50 rounded-2xl' >
+        <div id="ring" className='w-full h-max p-4 grid grid-cols-3 place-items-center bg-slate-400/50 rounded-2xl' >
 
             {   
                 Array.from({ length: cirlceCount }).map((__, i) => {
-                    return <div key={i} className='w-max h-max'>
+                    return <div key={i} className='w-max h-max '>
                         <div title='menu' className="w-[100px] h-[100px] rounded-full flex items-center justify-center">
                             <Link id={item[i].id} className='w-full h-full rounded-full flex items-center justify-center border-4 border-transparent hover:border-cyan-300 overflow-hidden' href={item[i].href}>
                                 <Image className='w-full h-full object-fit' src={`${item[i].imgSrc}`} width={125} height={125} alt="project" />
