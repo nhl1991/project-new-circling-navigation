8807d8fda282d80bd3d0aa9a89f2d1b73f35e790
nhl1991 <laslark1991@gmail.com>
2025-03-28 18:19:21 +0900

    updated component for mobile.

---
+++ src/app/components/circleNavigationMobile.tsx
@@ -0,0 +1,30 @@
+'use client'
+import styles from '@/app/page.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import { WheelEvent } from 'react';
+import item from '@/app/lib/items.json'
+
+export default function NavigationMobile() {
+
+    const cirlceCount = 8; // Just change this value to add more circle and adjust size in page.module.css file
+
+    return (
+        <div id="ring" className='w-full h-max p-4 grid grid-cols-3 items-center justify-center bg-slate-400/50 rounded-2xl' >
+
+            {   
+                Array.from({ length: cirlceCount }).map((__, i) => {
+                    return <div key={i} className='w-max h-max'>
+                        <div title='menu' className="w-[100px] h-[100px] rounded-full flex items-center justify-center">
+                            <Link id={item[i].id} className='w-full h-full rounded-full flex items-center justify-center border-4 border-transparent hover:border-cyan-300 overflow-hidden' href={item[i].href}>
+                                <Image className='w-full h-full object-fit' src={`${item[i].imgSrc}`} width={125} height={125} alt="project" />
+                            </Link>
+                        </div>
+                        <p className='w-full text-center p-2 text-white font-bold'>{item[i].id}</p>
+                    </div>
+
+                })
+            }
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react';
 import Navigation from './components/circleNavigation';
+import NavigationMobile from './components/circleNavigationMobile';
 
 export default function Home() {
 
@@ -25,8 +26,9 @@ export default function Home() {
   if (isMobile)
     return (
 
-      <div className={`w-full h-full flex py-2 px-4 justify-center items-center bg-[url(/background/background-m.jpg)] bg-cover bg-no-repeat rounded`}>
-        <p className='w-full py-2 px-4 bg-slate-400/75 rounded-2xl text-white  text-xl text-center'>This Site is only available on Desktop.</p>
+      <div className={`w-full h-full flex p-1 justify-center items-center bg-[url(/background/background-m.jpg)] bg-cover bg-no-repeat rounded`}>
+        {/* <p className='w-full py-2 px-4 bg-slate-400/75 rounded-2xl text-white  text-xl text-center'>This Site is only available on Desktop.</p> */}
+        <NavigationMobile />
       </div>
     )
   else
